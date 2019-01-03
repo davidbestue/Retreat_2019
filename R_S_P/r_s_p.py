39ca0226daa7121bf21ae0e33dbd9a5028f54066
davidbestue <davidsanchezbestue@hotmail.com>
2019-01-03 12:32:49 +0100

    Update r_s_p.py

--- R_S_P/r_s_p.py
+++ R_S_P/r_s_p.py
@@ -19,19 +19,19 @@ def id_generator(size=6, chars=['r', 's', 'p']): #'r', 's', 'p'
 
 id_generator()
 
-N=100
+N=1000
 id_generator(N)
 
 a = id_generator(N)
 seq = [a[x] for x in range(0, N)]    
-seq = [int(seq[i]) for i in range(0, len(seq))]  
+#seq = [int(seq[i]) for i in range(0, len(seq))]  
 
 
 #### Direct distribution
-f_r = seq.count('r')
-f_s = seq.count('s')
-f_p = seq.count('p')
-chisquare([f_r, f_s, f_p], f_exp=[len(seq)/3, len(seq)/3, len(seq)/3])
+#f_r = seq.count('r')
+#f_s = seq.count('s')
+#f_p = seq.count('p')
+#chisquare([f_r, f_s, f_p], f_exp=[len(seq)/3, len(seq)/3, len(seq)/3])
 
 
 ### Acumulate randomness
@@ -47,34 +47,80 @@ m=3
 
 #### All Consecutive pairs
 
-n=2
-
-all_pairs=[]
-
-for i in range(0, n):
-    all_pairs.append( [seq[a]+ seq[a+1] for a in range(i,len(seq)-n,2) ] )
-
-
+s=""
+n = 6
+all_poss_comb = [s.join(seq[i: i+n ]) for i in range(0, len(seq) - (n-1)) ]
 
 #### Once you have the list of all the data
-all_p_list = list(itertools.chain.from_iterable(all_pairs))
-all_p = pd.DataFrame(all_p_list)
+all_p = pd.DataFrame(all_poss_comb)
 
 unique = all_p[0].unique()
-f_obs=np.zeros(m**n)
 
-for idx,i in enumerate(unique):
-    f_obs[idx] = all_p_list.count(i)
+if n<15:
+    f_obs=np.zeros(m**n)
+    
+    for idx,i in enumerate(unique):
+        f_obs[idx] = all_poss_comb.count(i)
+    
+    f_obs = list(f_obs)
+    
+    f_exp = [ len(all_poss_comb)/m**n for i in range(0, len(f_obs))]
+    
+    if chisquare(f_obs, f_exp)[1]<0.05:
+        Acumulate_randomness.append(m**n)
 
-f_obs = list(f_obs)
-
-f_exp = [ len(all_p_list)/m**n for i in range(0, len(f_obs))]
-
-if chisquare(f_obs, f_exp)[1]<0.05:
-    Acumulate_randomness.append(m**n)
-
+else: ## memory error
+    f_obs=[]
+    for i in unique:
+        f_obs.append(all_poss_comb.count(i))
+    
 
 
+#####################
+        ##################
+        ###################
+
+
+
+
+for n in [2,3]:
+
+f_obs_filt_rep=[]
+for i in unique:
+    f_obs_filt_rep.append(all_poss_comb.count(i))
+    
+
+if len(pd.DataFrame(f_obs_filt_rep)[0].unique()) == 1:  #no repeted subsequences
+    print( 'No rep freq for ' + str(n))
+
+else: #there are repeted subsequences
+    
+    
+    if n<15:
+        
+        f_obs=np.zeros(m**n)
+        
+        for idx,i in enumerate(unique):
+            f_obs[idx] = all_poss_comb.count(i)
+        
+        f_obs = list(f_obs)
+        
+        f_exp = [ len(all_poss_comb)/m**n for i in range(0, len(f_obs))]
+        
+        print(chisquare(f_obs, f_exp)[1])
+        if chisquare(f_obs, f_exp)[1]<0.05:
+            Acumulate_randomness.append(m**n)
+    
+    else: ## memory error
+        print( 'Rep of ' + str(n))
+        Acumulate_randomness.append(m**n)
+
+
+
+    
+    
+    
+    
 
 
 ### trios
@@ -87,5 +133,22 @@ for i in range(0, n):
 
 list(itertools.chain.from_iterable(all_trios))
 
+
+
+##########################################
+##########################################
+
+
+
+s=""
+
+n = 50
+all_poss_comb = [s.join(seq[i: i+n ]) for i in range(0, len(seq) - (n-1)) ]
+
+
+
+print(combinations[0][-3:])
+print(combinations[1][-3:])
+print(seq[-6:])
 
 
