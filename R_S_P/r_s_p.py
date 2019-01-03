0bc555e3af1dab556071efd6e27052e178114857
davidbestue <davidsanchezbestue@hotmail.com>
2019-01-03 13:10:47 +0100

    Update r_s_p.py

--- R_S_P/r_s_p.py
+++ R_S_P/r_s_p.py
@@ -25,21 +25,25 @@ id_generator(N)
 a = id_generator(N)
 seq = [a[x] for x in range(0, N)]    
 
+
 ### Acumulate randomness
 Acumulate_randomness = []
-
+control_randomness =[]
 ### formula to now all the possible combinations
 #m**n (m is 3: 'p, r, s', and n is the group (2, 3.... (n-1)))
 m=3
 
 
-for n in range(1, N/2): ##length of grouping
+for n in range(1, int(N/2)): ##length of grouping
     
     ### All possible combinations   
     s=""
     all_poss_comb = [s.join(seq[i: i+n ]) for i in range(0, len(seq) - (n-1)) ]
     all_p = pd.DataFrame(all_poss_comb)
     unique = all_p[0].unique()
+    
+    ##control
+    #control_randomness.append(m**n * 1/1)
     
     ### start checking for repeted sequences
     f_obs_filt_rep=[]
@@ -48,8 +52,8 @@ for n in range(1, N/2): ##length of grouping
         
     #no repeted subsequences
     if len(pd.DataFrame(f_obs_filt_rep)[0].unique()) == 1:  
-        print( 'No rep freq for ' + str(n))
-        Acumulate_randomness.append(m**n * 1/1)
+        print( 'No rep freq of ' + str(n))
+        #Acumulate_randomness.append(m**n * 1/1)
     
     #there are repeted subsequences
     else: 
@@ -65,22 +69,31 @@ for n in range(1, N/2): ##length of grouping
             
             f_exp = [ len(all_poss_comb)/m**n for i in range(0, len(f_obs))]
             
-            print(chisquare(f_obs, f_exp))
-            stat, p_value = chisquare(f_obs, f_exp)[1]
+            #print(chisquare(f_obs, f_exp))
+            stat, p_value = chisquare(f_obs, f_exp)
+            
             Acumulate_randomness.append(m**n * 1/p_value)
-            if p_value <0.05:
-                print( 'Rep for ' + str(n))
+            control_randomness.append(m**n * 1/1)
+            print( 'Rep of ' + str(n) + ', p_value = ' + str(p_value))
                 
                 
         
         else: ## memory error
             print( 'Rep of ' + str(n))
             Acumulate_randomness.append(m**n * 1/0.05)
+            control_randomness.append(m**n * 1/1)
 
 
 
-    
-    
+
+
+control =  sum(control_randomness)
+rand_seq = round( sum(Acumulate_randomness) - control, 3)
+
+print('')
+print('Randomness of the sequence: ')
+##### Bigger the number, less random is!!!!!
+print(rand_seq)
     
     
 
