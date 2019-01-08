32991eccc5199dd4a311bc6bd1de053510858b52
davidbestue <davidsanchezbestue@hotmail.com>
2019-01-08 11:06:53 +0100

    new

--- R_S_P/task_2_players.py
+++ R_S_P/task_2_players.py
@@ -15,52 +15,115 @@ import numpy as np
 from itertools import chain
 import psychopy.visual
 import psychopy.event
-#from psychopy import gui  ### the gui makes competence with easygui! you do not need it!
 from psychopy import visual, core, event
 import pandas as pd
 import os
 import glob
-
 import easygui
 from easygui import multenterbox
+import pyglet
 
 
 #Dialogue box for subject name
 screen_dim =[1600, 900]
-min_bet_perc=1
-max_bet_perc=30
 
+rounds=2
+
+black=[-1,-1,-1]
 
 root_imgs = os.getcwd() + '\\images\\' 
 
 msg = 'Names '
 title = "Subjects information"
-fieldNames = ["Player1", "Player2"]
+fieldNames = ["Player_left", "Player_right"]
 fieldValues = multenterbox(msg,title, fieldNames)
 player1 = fieldValues[0]
 player2 = fieldValues[1]
 
 
-win = psychopy.visual.Window(size=screen_dim, units="pix", pos=(0, 0), fullscr=False, allowGUI=True)
+win = psychopy.visual.Window(size=screen_dim, units="pix", pos=(0, 0), fullscr=True, allowGUI=True)
+key=pyglet.window.key
+keyboard = key.KeyStateHandler()
+win.winHandle.push_handlers(keyboard)
 
 
 
-for trial in range(0, len(stims)):
+for trial in range(0, rounds):
     
     #Trial number
-    trial_number=visual.TextStim(win=win, text= str(trial) + '/' +str(len(stims)) , pos=[screen_dim[0]/3, screen_dim[1]/2.3], wrapWidth=screen_dim[0]/2, color=[1,1,1], units='pix', height=screen_dim[1]/25) 
+    trial_number=visual.TextStim(win=win, text= str(trial) + '/' +str(rounds) , pos=[screen_dim[0]/3, screen_dim[1]/2.3], wrapWidth=screen_dim[0]/2, color=[1,1,1], units='pix', height=screen_dim[1]/25) 
     trial_number.draw()
     
-    #Select the images
-    card1 = path + '/Vector-Playing-Cards-master/cards-png/' + stims.iloc[trial].card1
-    card2 = path + '/Vector-Playing-Cards-master/cards-png/' + stims.iloc[trial].card2
+    #Names players
+    pl1=visual.TextStim(win=win, text=player1, pos=[ -0.25* screen_dim[0],  0.25* screen_dim[1]], wrapWidth=screen_dim[0]/2, color=[1,1,1], units='pix', height=screen_dim[1]/15) 
+    pl2=visual.TextStim(win=win, text=player2, pos=[ +0.25* screen_dim[0],  0.25* screen_dim[1]], wrapWidth=screen_dim[0]/2, color=[1,1,1], units='pix', height=screen_dim[1]/15) 
+    pl1.draw()
+    pl2.draw()
     
-    image1 = psychopy.visual.ImageStim(win=win, image=card1 , units='pix',size=(250,350), ori=0, pos=(150, 100))                                                                                                                                                   
-    image2 = psychopy.visual.ImageStim(win=win, image=card2 , units='pix',size=(250,350), ori=0, pos=(-150, 100))                                                     
     
+    #play instruction
+    play_txt=visual.TextStim(win=win, text='Play!', pos=[0, -0.1* screen_dim[1]], wrapWidth=screen_dim[0]/2, color=black, units='pix', height=screen_dim[1]/10) 
+    play_txt.draw()
+    
+    p1_dec = 'None'
+    p2_dec = 'None'
+    
+    while p1_dec == 'None' or p2_dec == 'None':
+        
+        ##      
+        print(p1_dec, p2_dec)
+        #
+        if keyboard[key.A]:
+            p1_dec = 'left'
+        
+        if keyboard[key.L]:
+            p2_dec = 'right'      
+    
+    
+    print(p1_dec, p2_dec)
+        
+        
+    
+    
+    #scape loop
+    if keyboard[key.E]:
+        win.close()
+    
+    
+    #######
+    
+    if mouse_click[0]: #keyboard[key.A]:
+        mov_ang=1
+
+    
+    rock = root_imgs + 'rock.png'
+    rock_img = psychopy.visual.ImageStim(win=win, image=rock , units='pix',size=(250,350), ori=0, pos=(150, 100))                                                           
     #Draww and display
-    image1.draw()                                                                                                                                                 
-    image2.draw()
+    rock_img.draw()                                                                                                                                                 
     win.flip()
     
     core.wait(1)
+    
+    
+
+
+
+rock = root_imgs + 'rock.png'
+rock_img = psychopy.visual.ImageStim(win=win, image=rock , units='pix',size=(screen_dim[1]/2.5,screen_dim[1]/2.5), ori=0, pos=(150, 100))                                                           
+rock_img.draw()                                                                                                                                                 
+win.flip()
+
+scissors = root_imgs + 'scissor.png'
+scissors_img = psychopy.visual.ImageStim(win=win, image=scissors , units='pix',size=(screen_dim[1]/2.5,screen_dim[1]/2.5), ori=0, pos=(150, 100))                                                           
+scissors_img.draw()                                                                                                                                                 
+win.flip()
+
+
+paper = root_imgs + 'paper.png'
+paper_img = psychopy.visual.ImageStim(win=win, image=paper , units='pix',size=(screen_dim[1]/2.5,screen_dim[1]/2.5), ori=0, pos=(150, 100))                                                           
+paper_img.draw()                                                                                                                                                 
+win.flip()
+
+
+
+
